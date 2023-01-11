7607463b95034a9376a659918527fc78371a8fdb
К Б <yabkn@outlook.com>
2023-01-11 20:07:43 +0700

    begin command parttern

--- LimitedSizeStack/ListModel.cs
+++ LimitedSizeStack/ListModel.cs
@@ -23,40 +23,132 @@ namespace TodoApplication
     {
         public List<TItem> Items { get; }
         public int Limit;
-        public LimitedSizeStack<List<object>> History { get; }
+        //public LimitedSizeStack<List<object>> History { get; }
+        Invoker invoker;
+        Receiver receiver;
 
-        public ListModel(int limit)
+       public ListModel(int limit)
         {
             Items = new List<TItem>();
-            History= new LimitedSizeStack<List<object>>(limit);
+            //History= new LimitedSizeStack<List<object>>(limit);
             Limit = limit;
+            invoker = new Invoker();
+            receiver = new Receiver();
         }
 
         public void AddItem(TItem item)
         {
-            Items.Add(item);
-            History.Push(new List<object> { "add", item });
+            //Items.Add(item);
+            //History.Push(new List<object> { "add", item });
+            invoker.Add(item);
         }
 
         public void RemoveItem(int index)
         {
-            History.Push(new List<object> { "rem", index, Items[index] });
-            Items.RemoveAt(index);
+            //History.Push(new List<object> { "rem", index, Items[index] });
+            //Items.RemoveAt(index);
+            invoker.RemoveAt(index);
         }
 
         public bool CanUndo()//возвращает true, если на данный момент история действий не пуста
         {
-            return History.Count>0;
+            //return false;
+            return History.Count > 0;
         }
 
-        public void Undo() //отменяет последнее действие из истории
+
+    }
+
+    abstract class Command // абстрактный класс с командами
+
+    {
+        public abstract void AddItem();
+        public abstract void RemoveItem();
+        public abstract void Undo();
+    }
+
+    class ConcreteCommand : Command
+    {
+        Receiver receiver; //подключаем исполнителя команды
+        public LimitedSizeStack<List<object>> History { get; }
+
+        public ConcreteCommand(Receiver r) //указваем какой конкретно исполнитель
+        {
+            receiver = r;
+        }
+        public override void AddItem() //прописана реализация команды execute т.е. чего конктретно то делаем
+        {
+            receiver.Add();
+        }
+
+        public override void RemoveItem() //прописана реализация команды Undo т.е. чего конктретно то делаем
+        {
+            receiver.Off();
+        }
+
+        
+
+        public override void Undo() //отменяет последнее действие из истории
         {
 
             var command = History.Pop();
             if (command[0].ToString() == "rem")
                 Items.Insert((int)command[1], (TItem)command[2]);
             if (command[0].ToString() == "add")
-                Items.RemoveAt(Items.Count-1);
+                Items.RemoveAt(Items.Count - 1);
+        }
+
+    }
+
+    // Receiver - получатель команды. Определяет действия, которые должны выполняться в результате запроса
+    class Receiver
+    {
+        public void Add(TItem item)
+        {
+            Items.Add(item); ;
+        }
+
+        public void Remove(int index)
+        {
+            Items.RemoveAt(index);
         }
     }
-}
+    // Invoker - инициатор команды- вызывает команду для выполнения определенного запроса
+    class Invoker
+    {
+        Command command;//завязываем какие у нас команды будут выполняться. По идее здесь должны быть расписаны все команды по отдельности
+        
+        public void SetCommand(Command c)
+        {
+            command = c;
+        }
+        public void Add(TItem item)
+        {
+            command.Execute();
+        }
+        public void PressButtonCancel(int index)
+        {
+            command.Undo();
+        }
+    }
+
+
+    class Client
+    {
+        static void Main() //клиент - создает команду и устанавливает ее получателя с помощью метода SetCommand()
+                           //есть инициатор команды - класс Pult, некий прибор - пульт, управляющий телевизором.И есть получатель команды - класс TV,
+                           //представляющий телевизор. В качестве клиента используется класс Clien
+        {
+            Pult invoker = new Pult();// подключаем инициатор через которого будем посылать команду
+            TV receiver = new TV();// подключаем конечного выполнителя команды
+            ConcreteCommand command = new ConcreteCommand(receiver);//передаем данные что команды будут отрабатываться именно на
+                                                                    //таком выполнителе - телевизоре/
+                                                                    //связывает объекты исполнителей с созданными командами (инкапсулируем исполнителя)
+            invoker.SetCommand(command);//связывает объекты отправителей с созданными командами (инкапсулируем команды)
+            //по идее здесь должен писаться полный список команд с параметрами запуска как для микроволновки, а не одна команда
+            //две верхних строки понятней было бы написать так invoker.SetCommand(new ConcreteCommand(receiver));
+            invoker.PressButtonRun();//команда на включение телевизора
+            invoker.PressButtonCancel();
+
+        }
+    }
