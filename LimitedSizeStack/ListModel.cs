703afc0639f506b1b4b51b340b0402a091a89282
К Б <yabkn@outlook.com>
2023-01-12 12:55:25 +0700

    command pattern have done

--- LimitedSizeStack/ListModel.cs
+++ LimitedSizeStack/ListModel.cs
@@ -15,66 +15,67 @@ using System.Collections.Generic;
 Метод CanUndo возвращает true, если на данный момент история действий не пуста, то есть если вызов Undo будет корректным. 
 Иначе метод должен вернуть false.
 Проверить корректность своего решения можно на модульных тестах из класса ListModel_Should и ListModel_PerformanceTest.
-*/
+Если хотите, можете воспользоваться классическим объектно-ориентированным шаблоном Команда. Однако для сдачи данной задачи, точно следовать этому шаблону необязательно
+
+ */
 
 namespace TodoApplication
 {
     public class ListModel<TItem>
     {
-        //public List<TItem> Items { get; }
-        //public int Limit;
-        //public LimitedSizeStack<List<object>> History { get; }
-        Invoker invoker;
-        Receiver receiver;
+        public List<TItem> Items { get; }
+        public int Limit;
+        Invoker<TItem> invoker;
+        Receiver<TItem> receiver;
 
-       public ListModel(int limit)
+        public ListModel(int limit)
         {
-            //History= new LimitedSizeStack<List<object>>(limit);
-            //Items = new List<TItem>();
-            //Limit = limit;
-            invoker = new Invoker();
-            receiver = new Receiver(limit);
-            invoker.SetCommand(new ConcreteCommand(receiver, limit));
+            Items = new List<TItem>();
+            Limit = limit;
+            invoker = new Invoker<TItem>();
+            receiver = new Receiver<TItem>(Items);
+            invoker.SetCommand(new ConcreteCommand<TItem>(receiver, limit));
         }
 
         public void AddItem(TItem item)
         {
             Items.Add(item);
-            //History.Push(new List<object> { "add", item });
-            //invoker.Add(item);
             invoker.Execute(new List<object> { "add", item });
         }
 
         public void RemoveItem(int index)
         {
-            //History.Push(new List<object> { "rem", index, Items[index] });
-            Items.RemoveAt(index);
-            //invoker.RemoveAt(index);
             invoker.Execute(new List<object> { "rem", index, Items[index] });
+            Items.RemoveAt(index);
         }
 
         public bool CanUndo()//возвращает true, если на данный момент история действий не пуста
         {
-            //return false;
             return invoker.CanUndo();
+        }
+
+        public void Undo() //отменяет последнее действие из истории
+        {
+            invoker.Undo();
         }
 
 
     }
 
-    interface ICommand
+    interface ICommand<TItem>
     {
-        void Execute();
-        void UnExecute();
+        void Execute(List<object> obj);
+        bool CanUndo();
+        void Undo();
     }
 
-    class ConcreteCommand : ICommand
+    class ConcreteCommand<TItem> : ICommand<TItem>
     {
-        Receiver receiver; //подключаем исполнителя команды
+        Receiver<TItem> receiver ; //подключаем исполнителя команды
         public LimitedSizeStack<List<object>> History { get; }
-       
 
-        public ConcreteCommand(Receiver r, int limit) //указваем какой конкретно исполнитель
+
+        public ConcreteCommand(Receiver<TItem> r, int limit) //указваем какой конкретно исполнитель
         {
             receiver = r;
             History = new LimitedSizeStack<List<object>>(limit);
@@ -82,60 +83,47 @@ namespace TodoApplication
         public void Execute(List<object> obj) //string str, TItem item) //прописана реализация команды execute т.е. чего конктретно то делаем
         {
             History.Push(obj);
-            //receiver.Add();
-        }
-
-        public void UnExecute() //прописана реализация команды Undo т.е. чего конктретно то делаем
-        {
-            receiver.Off();
         }
 
         public bool CanUndo()//возвращает true, если на данный момент история действий не пуста
         {
-            //return false;
             return History.Count > 0;
         }
-
-
-
         public void Undo() //отменяет последнее действие из истории
         {
 
             var command = History.Pop();
             if (command[0].ToString() == "rem")
-                Items.Insert((int)command[1], (TItem)command[2]);
+                receiver.Insert((int)command[1], command[2]);
             if (command[0].ToString() == "add")
-                Items.RemoveAt(Items.Count - 1);
+                receiver.Remove();
         }
 
     }
 
     // Receiver - получатель команды. Определяет действия, которые должны выполняться в результате запроса
-    class Receiver
+    class Receiver<TItem>
     {
         public List<TItem> Items { get; }
-        public int Limit;
-        TItem item = new ListModel<TItem>();
-        public Receiver(int limit)
+        public Receiver(List<TItem> items)
         {
-            Items = new List<TItem>();
-            Limit = limit;
+            Items = items;
         }
-        public void Add(TItem item)
+        public void Insert(int position, object item)
         {
-            Items.Add(item); ;
+            Items.Insert(position, (TItem)item);
         }
 
-        public void Remove(int index)
+        public void Remove()
         {
-            Items.RemoveAt(index);
+            Items.RemoveAt(Items.Count - 1);
         }
     }
     // Invoker - инициатор команды- вызывает команду для выполнения определенного запроса
-    class Invoker
+    class Invoker<Titem>
     {
-        ICommand command;//завязываем какие у нас команды будут выполняться. По идее здесь должны быть расписаны все команды по отдельности
-        public void SetCommand(ICommand c)
+        ICommand<Titem> command;//завязываем какие у нас команды будут выполняться. По идее здесь должны быть расписаны все команды по отдельности
+        public void SetCommand(ICommand<Titem> c)
         {
             command = c;
         }
@@ -143,29 +131,16 @@ namespace TodoApplication
         {
             command.Execute(obj);
         }
-        public void CanUndo(List<object> obj)
+        public bool CanUndo()
         {
-            command.CanUndo();
+            return command.CanUndo();
+        }
+
+        public void Undo() //отменяет последнее действие из истории
+        {
+            command.Undo();
         }
     }
 
 
-    class Client
-    {
-        static void Main() //клиент - создает команду и устанавливает ее получателя с помощью метода SetCommand()
-                           //есть инициатор команды - класс Pult, некий прибор - пульт, управляющий телевизором.И есть получатель команды - класс TV,
-                           //представляющий телевизор. В качестве клиента используется класс Clien
-        {
-            Pult invoker = new Pult();// подключаем инициатор через которого будем посылать команду
-            TV receiver = new TV();// подключаем конечного выполнителя команды
-            ConcreteCommand command = new ConcreteCommand(receiver);//передаем данные что команды будут отрабатываться именно на
-                                                                    //таком выполнителе - телевизоре/
-                                                                    //связывает объекты исполнителей с созданными командами (инкапсулируем исполнителя)
-            invoker.SetCommand(command);//связывает объекты отправителей с созданными командами (инкапсулируем команды)
-            //по идее здесь должен писаться полный список команд с параметрами запуска как для микроволновки, а не одна команда
-            //две верхних строки понятней было бы написать так invoker.SetCommand(new ConcreteCommand(receiver));
-            invoker.PressButtonRun();//команда на включение телевизора
-            invoker.PressButtonCancel();
-
-        }
-    }
+}    
